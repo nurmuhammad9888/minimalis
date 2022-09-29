b3f41e7cb0d6aab8b486a6e3bd420051ed53e704
nurmuhammad <juraqobilovnurmuhammad9888@gmail.com>
2022-09-29 09:39:03 +0500

    Add

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ let elNav = document.querySelector(".sitenav");
 
 elBtn.addEventListener("click", function(){
     elNav.classList.toggle("sitenav-show");
-    document.body.classList.toggle("bodyshow")
+    document.body.classList.toggle("bodyshow");
+    elBtn.classList.toggle("site-header__btn-show")
 })
